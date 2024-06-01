7998dfcf612372a4843eaa8eb3244c8f2402b2a3
ronaldserna <ronald.serna@gmail.com>
2024-05-31 21:32:44 -0500

    cambio 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola, World!");
-Console.WriteLine("Hola, Marte!");
-Console.WriteLine("Hola, VENUS!");
